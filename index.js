9e67e14e2ac60f78639891ade3793e88736d3a40
Hugo Derre <hugo.d83@outlook.fr>
2023-03-15 14:06:25 +0100

    fix: port 3000 instead 3001

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require( 'express' )
+const bodyParser = require( 'body-parser' )
 const expressApp = express()
 const App = require( './src/app.js' )
 
-expressApp.listen( 3001, () => console.log('Listen 3001') )
-expressApp.use(bodyParser.json());
+expressApp.listen( 3000, () => console.log( 'Listen 3000' ) )
+expressApp.use( bodyParser.json() );
 
-new App(expressApp)
+new App( expressApp )
