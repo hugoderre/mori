0e2f0895390359ea6f5539eb111ffbd74fbfb7b1
Hugo Derre <hugo.d83@outlook.fr>
2023-04-12 22:14:20 +0200

    feat: remove french tts support

--- Core/src/tts/voicemaker.js
+++ Core/src/tts/voicemaker.js
@@ -1,13 +1,11 @@
 const { spawn } = require( 'node:child_process' )
 const { VoiceMaker, VoiceMakerRequest } = require( 'voicemaker' )
 const SubTitle = require( './subtitle.js' )
-const LanguageDetect = require( 'languagedetect' );
 
 class VoiceMakerAPI {
 	constructor( openaiClientInstance ) {
 		this.openaiClientInstance = openaiClientInstance
 		this.voiceMakerEngine = new VoiceMaker()
-		this.lngDetector = new LanguageDetect();
 		this.subTitleEngine = new SubTitle( 3002 )
 		this.subTitleEngine.initServer()
 	}
@@ -17,21 +15,15 @@ class VoiceMakerAPI {
 			throw new Error( 'No message provided.' )
 		}
 		const voiceMakerRequest = new VoiceMakerRequest( message )
-		const detectedLanguage = this.lngDetector.detect( message, 1 )[ 0 ][ 0 ]
-		if ( detectedLanguage === 'french' ) {
-			voiceMakerRequest.setVoice( "ai3-fr-FR-Emmy" )
-			voiceMakerRequest.pitch = "8%"
-		} else {
-			voiceMakerRequest.setVoice( "ai3-Nova" )
-			voiceMakerRequest.pitch = "-2%"
-			voiceMakerRequest.speed = "100%"
-		}
+		voiceMakerRequest.setVoice( "ai3-Nova" )
+		voiceMakerRequest.pitch = "6%"
+		voiceMakerRequest.speed = "100%"
 		voiceMakerRequest.volume = 10
 		const outputPath = await this.voiceMakerEngine.getTts( voiceMakerRequest )
 		this.subTitleEngine.send( message )
 		const vlcProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', outputPath ] )
 		vlcProcess.on( 'close', ( code ) => {
-			this.openaiClientInstance.isCompletionInProcess = false
+			this.openaiClientInstance.isMoriSpeaking = false
 			console.log( 'TTS DONE' )
 		} )
 	}
