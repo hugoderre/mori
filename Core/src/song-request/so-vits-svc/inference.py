7db5a0f05e3ffb1f5c7d36e342b1b2ac55de8d9d
Hugo Derre <hugo.d83@outlook.fr>
2023-04-15 16:12:50 +0200

    feat(song-request): merge instrumental + voice to avoid delay when rendering

--- Core/src/song-request/so-vits-svc/inference.py
+++ Core/src/song-request/so-vits-svc/inference.py
@@ -23,27 +23,30 @@ for file in os.listdir(split_audio_folder):
 for file in os.listdir(processed_audio_folder):
 	os.remove(os.path.join(processed_audio_folder, file))
 
-input_file = os.path.join(parent_dir, "htdemucs\\base_song\\vocals.wav")
-output_file = os.path.join(parent_dir, "htdemucs\\base_song\\vocals.out.wav")
+input_acapella_file_path = os.path.join(parent_dir, "htdemucs\\base_song\\vocals.wav")
+output_acapella_file_path = os.path.join(parent_dir, "htdemucs\\base_song\\vocals.out.wav")
+output_instrumental_file_path = os.path.join(parent_dir, "htdemucs\\base_song\\no_vocals.wav")
+output_full_file_path = os.path.join(parent_dir, "htdemucs\\base_song\\full.wav")
 
 speaker = "mori"
 model_path = os.path.join(parent_dir, "models\\G_4000.pth")
 config_path = os.path.join(parent_dir, "models\\config.json")
 
 # num_parts = 8
-overlap_seconds = 1
-seconds_per_part = 25
+overlap_seconds = 0.125
+seconds_per_part = 8
 
 def main():
     timestamp = datetime.now()
-    split_audio_file(input_file, split_audio_folder, seconds_per_part, overlap_seconds)
+    split_audio_file(input_acapella_file_path, split_audio_folder, seconds_per_part, overlap_seconds)
     infer_audio_files(split_audio_folder, processed_audio_folder)
-    crossfade_audio_files(processed_audio_folder, output_file, overlap_seconds)
+    crossfade_audio_files(processed_audio_folder, output_acapella_file_path, overlap_seconds)
+    overlay_instrumental(output_acapella_file_path, output_instrumental_file_path, output_full_file_path)
     duration = datetime.now() - timestamp
     print(f"Elapsed duration: {duration}")
 
-def split_audio_file(input_file_path, output_folder_path, seconds_per_part, overlap_seconds):
-    audio = AudioSegment.from_file(input_file_path)
+def split_audio_file(input_acapella_file_path, output_folder_path, seconds_per_part, overlap_seconds):
+    audio = AudioSegment.from_file(input_acapella_file_path)
     audio_length = len(audio)
     num_parts = (audio_length // (seconds_per_part*1000)) + 1
     part_length = audio_length // num_parts
@@ -77,14 +80,20 @@ def infer_audio_files(input_folder_path, output_folder_path):
     p = subprocess.Popen(args)
     p.wait()
 
-def crossfade_audio_files(input_folder_path, output_file_path, overlap_seconds):
+def crossfade_audio_files(input_folder_path, output_acapella_file, overlap_seconds):
     input_files = os.listdir(input_folder_path)
     input_files.sort(key=lambda val: int(re.findall(r"(?<=_)[0-9]+(?=\.)", val)[0]))
     combined_audio = AudioSegment.from_file(os.path.join(input_folder_path, input_files[0]))
     for file_name in input_files[1:]:
         part = AudioSegment.from_file(os.path.join(input_folder_path, file_name))
         combined_audio = combined_audio.append(part, crossfade=overlap_seconds * 2 * 1000)
-    combined_audio.export(output_file_path, format="wav")
+    combined_audio.export(output_acapella_file, format="wav")
+    
+def overlay_instrumental(vocal_file, output_instrumental_file_path, output_full_file_path):
+    vocal_audio = AudioSegment.from_file(vocal_file)
+    instrumental_audio = AudioSegment.from_file(output_instrumental_file_path)
+    combined_audio = vocal_audio.overlay(instrumental_audio)
+    combined_audio.export(output_full_file_path, format="wav")
 
 if __name__ == '__main__':
     main()
--- Core/src/song-request/songRequest.js
+++ Core/src/song-request/songRequest.js
@@ -151,7 +151,7 @@ class SongRequest {
 
 	async startSong() {
 		return new Promise( ( resolve, reject ) => {
-			const singingProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', '--aout=waveout', '--waveout-audio-device=VX238 (NVIDIA High Definition A ($1,$64)', 'src/song-request/htdemucs/base_song/no_vocals.wav' ] )
+			const singingProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', '--aout=waveout', '--waveout-audio-device=VX238 (NVIDIA High Definition A ($1,$64)', 'src/song-request/htdemucs/base_song/full.wav' ] )
 			// const singingProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', '--aout=waveout', '--waveout-audio-device=VoiceMeeter Input (VB-Audio Voi ($1,$64)', 'src/song-request/htdemucs/base_song/no_vocals.wav' ] )
 			// const singingProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', '--aout=waveout', '--waveout-audio-device=Haut-parleurs (2- Focusrite USB ($1,$64)', 'src/song-request/htdemucs/base_song/no_vocals.wav' ] )
 			spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', '--aout=waveout', '--waveout-audio-device=VoiceMeeter Input (VB-Audio Voi ($1,$64)', 'src/song-request/htdemucs/base_song/vocals.out.wav' ] )
