d58b63b8e18cba9ab65475b1ad33c3b0d0e84dc3
Hugo Derre <hugo.d83@outlook.fr>
2023-04-22 23:15:14 +0200

    feat: now runTTS method return a promise

--- Core/src/tts/voicemaker.js
+++ Core/src/tts/voicemaker.js
@@ -11,20 +11,23 @@ class VoiceMakerAPI {
 	}
 
 	async runTTS( message ) {
-		if ( !message ) {
-			throw new Error( 'No message provided.' )
-		}
-		const voiceMakerRequest = new VoiceMakerRequest( message )
-		voiceMakerRequest.setVoice( "ai3-Nova" )
-		voiceMakerRequest.pitch = "8%"
-		voiceMakerRequest.speed = "100%"
-		voiceMakerRequest.volume = 10
-		const outputPath = await this.voiceMakerEngine.getTts( voiceMakerRequest )
-		this.subTitleEngine.send( message )
-		const vlcProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', outputPath ] )
-		vlcProcess.on( 'close', ( code ) => {
-			this.openaiClientInstance.isMoriSpeaking = false
-			console.log( 'TTS DONE' )
+		return new Promise( async ( resolve, reject ) => {
+			if ( !message ) {
+				return reject( 'No message provided.' )
+			}
+			const voiceMakerRequest = new VoiceMakerRequest( message )
+			voiceMakerRequest.setVoice( "ai3-Nova" )
+			voiceMakerRequest.pitch = "8%"
+			voiceMakerRequest.speed = "100%"
+			voiceMakerRequest.volume = 10
+			const outputPath = await this.voiceMakerEngine.getTts( voiceMakerRequest )
+			this.subTitleEngine.send( message )
+			const vlcProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', outputPath ] )
+			vlcProcess.on( 'close', ( code ) => {
+				this.openaiClientInstance.isMoriSpeaking = false
+				console.log( 'TTS DONE' )
+				resolve()
+			} )
 		} )
 	}
 }
