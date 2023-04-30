f41870c55877caa102c05f5b3324ad0dc2b9e12c
Hugo Derre <hugo.d83@outlook.fr>
2023-04-30 21:16:00 +0200

    feat: added try catch

--- Core/src/tts/voicemaker.js
+++ Core/src/tts/voicemaker.js
@@ -15,12 +15,28 @@ class VoiceMakerAPI {
 			if ( !message ) {
 				return reject( 'No message provided.' )
 			}
-			const voiceMakerRequest = new VoiceMakerRequest( message )
+
+			let voiceMakerRequest;
+			try {
+				voiceMakerRequest = new VoiceMakerRequest( message )
+			} catch ( error ) {
+				console.log( 'Error creating VoiceMakerRequest with message:', message )
+				return reject( error )
+			}
+
 			voiceMakerRequest.setVoice( "ai3-Nova" )
 			voiceMakerRequest.pitch = "8%"
 			voiceMakerRequest.speed = "100%"
 			voiceMakerRequest.volume = 10
-			const outputPath = await this.voiceMakerEngine.getTts( voiceMakerRequest )
+
+			let outputPath;
+			try {
+				outputPath = await this.voiceMakerEngine.getTts( voiceMakerRequest )
+			} catch ( error ) {
+				console.log( 'Error getting TTS with voiceMakerRequest' )
+				return reject( error )
+			}
+
 			this.subTitleEngine.send( message )
 			const vlcProcess = spawn( 'vlc', [ '--intf', 'dummy', '--no-video', '--play-and-exit', outputPath ] )
 			vlcProcess.on( 'close', ( code ) => {
