56610c750aef6f0fedf749adf68b2efc965a10b5
Hugo Derre <hugo.d83@outlook.fr>
2023-05-22 19:30:53 +0200

    feat: new subtitle font + improve readability

--- Core/index.js
+++ Core/index.js
@@ -1,10 +1,18 @@
 const express = require( 'express' )
-const bodyParser = require( 'body-parser' )
 const expressApp = express()
+const bodyParser = require( 'body-parser' )
 const App = require( './src/App.js' )
 
-expressApp.listen( 3000, () => console.log( 'Listen 3000' ) )
+expressApp.use( '/assets/fonts', ( req, res, next ) => {
+	res.setHeader( 'Access-Control-Allow-Origin', '*' );
+	res.setHeader( 'Access-Control-Allow-Methods', 'GET' );
+	res.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+	next();
+} );
+expressApp.use( '/assets', express.static( './assets' ) );
+
 expressApp.use( bodyParser.json() );
+expressApp.listen( 3000, () => console.log( 'Listen 3000' ) )
 
 const app = new App( expressApp )
 
